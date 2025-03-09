d3fd1bceee155da46ed4d10bd8255c81850bdae7
tiranga-creane-service <salmanansari910550@gmail.com>
2025-03-09 23:10:05 +0530

    change the location of the images

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -4,6 +4,7 @@ import SliderImage3 from './slider_image3.webp'
 import SliderImage4 from './slider_image4.jpg'
 import SliderImage5 from './slider_image5.webp'
 import BackgroundImage from './bg.jpg'
+
 import TcsLogo from './tcs_logo.jpg'
 import TcsLogoRB from './tcs_logo-removebg.png'
 import TcsLogo2 from './tcs_logo2.jpg'
